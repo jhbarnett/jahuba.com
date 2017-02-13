d743bb85554ad83090495eb4274e3df85aa69c21
Jason Barnett <jason@thebarnetts.ws>
2017-02-13 17:14:37 -0600

    sets up routing and base components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'blog',  component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'portfolio',     component: PortfolioComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module'
+
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NavComponent } from './nav/nav.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavComponent,
+    BlogComponent,
+    ContactComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
