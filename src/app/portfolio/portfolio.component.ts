6045673931f585cb17949566966a696ce84eea92
Jason Barnett <jason@thebarnetts.ws>
2017-02-15 15:16:53 -0600

    adds some gallery photos and renders carousel portfolio images

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,14 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import PROJECTS from './portfolio.data';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
+
 export class PortfolioComponent implements OnInit {
+  @ViewChild('wrap') wrap:ElementRef; 
+  @ViewChild('items') items:ElementRef; 
+  @ViewChild('slider') slider:ElementRef; 
+  
+  pos = 0;
+  
+  setTransform() {
+    this.items.nativeElement.style.transform = 'translate3d(' + (-this.pos * this.items.nativeElement.offsetWidth) + 'px,0,0)';
+  };
 
-  constructor() { }
+  prev() {
+    this.pos = Math.max(this.pos - 1, 0);
+    this.setTransform();
+    console.log("prev works", "position:", this.pos);
+  };
 
-  ngOnInit() { }
+  next() {
+    this.pos = Math.min(this.pos + 1, this.items.nativeElement.children.length - 1);
+    this.setTransform();
+    console.log("next works", "position:", this.pos);
+  };
+
+
+  onResize(event) {
+    this.setTransform;
+  }
+  
+  constructor(private sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+    console.log(this.projects)
+  }
+
+  projects = PROJECTS;
+
+  sanitize = ((imageURL) => {return `url(${this.sanitizer.bypassSecurityTrustUrl(imageURL)})`}).bind(this)
+  
 
 }
+
+
