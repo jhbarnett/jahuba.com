dae9e3277916fddc979190f15eee54c7980df358
Jason Barnett <jason@thebarnetts.ws>
2017-02-16 10:48:15 -0600

    creates email from contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+class Note {
+  name = ''
+  email = '';
+  message = '';
+}
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
+
 export class ContactComponent implements OnInit {
+  
+  constructor( ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  model = new Note()
+
+  mailForm(e) {
+   const subject = `${this.model.name} reaching out!`;
+   const body = `Message:\n\n${this.model.message}
+                 \n\nREPLY TO ${this.model.name} @ ${this.model.email}`;    
+   window.location.href = `mailto:jason@thebarnetts.ws?
+                           subject=${subject}
+                           &body=${body}`;
+   e.path[1].reset();        
   }
-
 }
