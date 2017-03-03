3510597f0aeaa07fba66d7da2e9ce87a21f44a64
Jason Barnett <jason@thebarnetts.ws>
2017-03-03 15:50:29 -0600

    completes professional bio and portfolio descriptions

--- src/app/portfolio/portfolio.data.ts
+++ src/app/portfolio/portfolio.data.ts
@@ -1,21 +1,21 @@
 const myProjects = [
   {
     title: "Line4Line",
-    description: "A web app for individuals to curate stories in tandem, one line at a time. Integrated MV* architecture alongside a MERN stack. Incorporated Socket.io to impart a seamless and responsive UX/UI. Used agile and Git workflows to coordinate a clean and reliable product",
+    description: "Line4Line is a web application for individuals to curate stories in tandem, one line at a time. Much like MadLibs, users contribute to a story with limited knowledge of the over-arching theme. It was built using React, Node, Express, MongoDB, and Socket.io.",
     image: "../../assets/line4line.png",
     github: "https://github.com/TEAManticore/Line4Line",
     link: "http://www.line4line.us"
   },
   {
     title: "Butter",
-    description: "Personal finance application to monitor credit accounts, spending, and more. Employed React and Redux to efficiently manipulate front-end responsiveness. Integrated LESS and Bootstrap to foster an engaging and intuitive UI/UX. Prioritized lean and agile methodologies to deliver a consumer-driven MVP",
+    description: "Butter is a personal finance application to monitor credit accounts, spending, and more. It provides a more pleasant and intuitive UI than spreadsheet as a resource for credit card churning. It was built with React, Redux, Node, Express, PostgreSQL, and Bootstrap.",
     image: "../../assets/butter.png",
     github: "https://github.com/ChurnDownForWhat/butter",
     link: "https://abutterapp.herokuapp.com"
   },
   {
     title: "iLuddite",
-    description: "A platform for Facebook friends to discover, preview, and review books. Synthesized data along RESTful standards to improve client-facing accessibility. Utilized MongoDB, Express, Node, and React to deliver a scalable SPA. Managed React lifecycles and stateless components for modular development.",
+    description: "iLuddite is a platform to discover, preview, and review books alongside your social networks. It integrates with the Goodreads and Google Books APIs to present the most utility and information to users. It was built using React, Node, Express, MongoDB, and Bootstrap.",
     image: "../../assets/iluddite.png",
     github: "https://github.com/TEAManticore/iLuddite-2.0",
     link: "https://i-luddite.herokuapp.com"
