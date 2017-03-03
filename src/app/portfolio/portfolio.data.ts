127e84e8489aed2c0efb104f6a7dea3ffb750690
Jason Barnett <jason@thebarnetts.ws>
2017-03-03 17:27:07 -0600

    optimizes photos to improve load time

--- src/app/portfolio/portfolio.data.ts
+++ src/app/portfolio/portfolio.data.ts
@@ -2,21 +2,21 @@ const myProjects = [
   {
     title: "Line4Line",
     description: "Line4Line is a web application for individuals to curate stories in tandem, one line at a time. Much like MadLibs, users contribute to a story with limited knowledge of the over-arching theme. It was built using React, Node, Express, MongoDB, and Socket.io.",
-    image: "../../assets/line4line.png",
+    image: "../../assets/line4line.jpg",
     github: "https://github.com/TEAManticore/Line4Line",
     link: "http://www.line4line.us"
   },
   {
     title: "Butter",
     description: "Butter is a personal finance application to monitor credit accounts, spending, and more. It provides a more pleasant and intuitive UI than spreadsheet as a resource for credit card churning. It was built with React, Redux, Node, Express, PostgreSQL, and Bootstrap.",
-    image: "../../assets/butter.png",
+    image: "../../assets/butter.jpg",
     github: "https://github.com/ChurnDownForWhat/butter",
     link: "https://abutterapp.herokuapp.com"
   },
   {
     title: "iLuddite",
     description: "iLuddite is a platform to discover, preview, and review books alongside your social networks. It integrates with the Goodreads and Google Books APIs to present the most utility and information to users. It was built using React, Node, Express, MongoDB, and Bootstrap.",
-    image: "../../assets/iluddite.png",
+    image: "../../assets/iluddite.jpg",
     github: "https://github.com/TEAManticore/iLuddite-2.0",
     link: "https://i-luddite.herokuapp.com"
   }
